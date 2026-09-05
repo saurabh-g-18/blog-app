a52522db459e5ec95c6472c372018df3acefb620
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-09-05 23:54:53 +0530

    Add SignUpPage layout with centered title text

--- lib/features/auth/presentation/pages/signup_pages.dart
+++ lib/features/auth/presentation/pages/signup_pages.dart
@@ -11,6 +11,17 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Sign Up',
+            style: TextStyle(
+              fontSize: 50,
+            ),
+            ),
+        ],
+      )
     );
   }
 }
