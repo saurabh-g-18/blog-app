b7796ea3bf812edf0652ac901d11cab60073b147
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-09-15 00:43:03 +0530

    Refactor main.dart to initialize Supabase and set up MultiBlocProvider for AuthBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/Presentation/pages/signup_pages.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(url: AppSecrets.supabaseUrl , anonKey: AppSecrets.supabaseAnonKey , );
-  runApp(const MyApp());
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBloc(
+            userSignUp: UserSignUp(
+              AuthRepositoryImpl(AuthRemoteDataSourceImpl(supabase.client)),
+            ),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,12 +37,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false, //removes the debug banner 
+      debugShowCheckedModeBanner: false, //removes the debug banner
       title: 'Blog App',
       theme: AppTheme.darkThemeMode,
       home: const LoginPage(),
     );
   }
-}     
+}
