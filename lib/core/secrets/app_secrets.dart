597e1edaa5d56a848529654c5924b62e4f705e85
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-09-15 01:21:46 +0530

    Format supabaseAnonKey declaration for consistency

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,4 +1,5 @@
 class AppSecrets {
   static const supabaseUrl = 'https://iquzulwkwuzyflzbkegb.supabase.co';
-  static const supabaseAnonKey = 'sb_publishable_CyW92_ynlxP2MARejq6_YA_TxbmClzL';
+  static const supabaseAnonKey =
+      'sb_publishable_CyW92_ynlxP2MARejq6_YA_TxbmClzL';
 }
