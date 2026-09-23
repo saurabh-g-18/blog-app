0490f9149b98c296ddb4509b06b88d181055763a
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-09-24 02:09:30 +0530

    Refactor AuthRepository to return User model instead of String for authentication methods

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,34 +1,37 @@
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-class AuthRepositoryImpl implements AuthRepository
-{
+class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
   const AuthRepositoryImpl(this.remoteDataSource);
   @override
-  Future<Either<Failure, String>> loginWithEmailPassword({required String email, required String password}) {
+  Future<Either<Failure, User>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
     // TODO: implement loginWithEmailPassword
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failure, String>> signUpWithEmailPassword({
+  Future<Either<Failure, User>> signUpWithEmailPassword({
     required String name,
-     required String email, 
-     required String password,
-     }) async {
-      try {
-        final userId = await remoteDataSource.signUpWithEmailPassword(
-          name: name,
-           email: email,
-            password: password
-            );
-            return right(userId);
-      } on ServerException catch (e) {
-        return left(Failure(e.message));
-      }
-   
-  }}
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,7 +21,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       );
       res.fold(
         (l) => emit(AuthFailure(l.message)),
-        (r) => emit(AuthSuccess(r)),
+        (user) => emit(AuthSuccess(user)),
       );
     });
   }
