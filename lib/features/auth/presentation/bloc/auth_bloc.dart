5b66f883351739ee34bf1c0944e6755b0d838864
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-09-24 15:57:53 +0530

    Refactor AuthBloc to include UserLogin dependency and streamline sign-up event handling

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/usecase/user_login.dart';
 import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
@@ -8,22 +9,26 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
-  AuthBloc({required UserSignUp userSignUp})
+  final UserLogin _userLogin;
+  AuthBloc({required UserSignUp userSignUp, required UserLogin userLogin})
     : _userSignUp = userSignUp,
+      _userLogin = userLogin,
       super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) async {
-      emit(AuthLoading());
-      final res = await _userSignUp(
-        UserSignUpParams(
-          email: event.email,
-          password: event.password,
-          name: event.name,
-        ),
-      );
-      res.fold(
-        (failure) => emit(AuthFailure(failure.message)),
-        (user) => emit(AuthSuccess(user)),
-      );
-    });
+    on<AuthSignUp>(_onAuthSignUp);
+  }
+
+  void _onAuthSignUp(AuthSignUp event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userSignUp(
+      UserSignUpParams(
+        email: event.email,
+        password: event.password,
+        name: event.name,
+      ),
+    );
+    res.fold(
+      (failure) => emit(AuthFailure(failure.message)),
+      (user) => emit(AuthSuccess(user)),
+    );
   }
 }
